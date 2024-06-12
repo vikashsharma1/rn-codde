808416fa5696fdac2606d1e5be302ba382fa689a
vikashsharma1 <vikash.sh@outlook.com>
2024-06-13 01:14:21 +0530

    SafeAreaView-in-rn

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet, Dimensions, useWindowDimensions } from 'react-native';
+import { Text, StyleSheet, useWindowDimensions, SafeAreaView } from 'react-native';
 
 
 export default function App() {
@@ -8,33 +8,16 @@ export default function App() {
   const windowHeight = useWindowDimensions().height;
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.box, { width: windowWidth > 768 ? '50%' : '70%' }]}>
+    <SafeAreaView style={styles.container}>
         <Text style={{ fontSize: windowWidth > 768 ? 42 : 24 }}>Welcome Vikash!</Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
-
 }
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'plum',
     alignItems: 'center',
-    justifyContent: 'center'
-  },
-  box: {
-    width: windowWidth > 768 ? '50%' : '70%',
-    height: '40%',
-    backgroundColor: 'lightblue',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    fontSize: windowWidth > 768 ? 42 : 24
   }
 })
