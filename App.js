482d7bf18f4e48e802f5fac86cc0cd14a2f77179
vikashsharma1 <vikash.sh@outlook.com>
2024-06-13 01:01:51 +0530

    dimension-in-rn

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, StyleSheet, Dimensions, useWindowDimensions } from 'react-native';
+
 
 export default function App() {
+
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box, { width: windowWidth > 768 ? '50%' : '70%' }]}>
+        <Text style={{ fontSize: windowWidth > 768 ? 42 : 24 }}>Welcome Vikash!</Text>
+      </View>
     </View>
   );
+
 }
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'plum',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  box: {
+    width: windowWidth > 768 ? '50%' : '70%',
+    height: '40%',
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: windowWidth > 768 ? 42 : 24
+  }
+})
